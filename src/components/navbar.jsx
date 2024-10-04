c75a40a7e2542e88b4946016318d59292368323d
Denitsa Slavkova <deni9309@gmail.com>
2024-10-04 21:51:19 +0300

    Add additional static files

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import { appleImg } from '../utils'
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="apple" height={18} width={14} />
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
